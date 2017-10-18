af138bfca5de00e171450ad3317a8ccc3292d356
Adrian <kmhorn1992@icloud.com>
2017-10-18 13:36:29 -0500

    first commit

---
+++ list.js
@@ -0,0 +1,37 @@
+
+$.get('https://www.reddit.com/r/halloween/.json')
+    .then(function (success) {
+        var grabPosts = contentInfo(success);
+
+        grabPosts.forEach(function (redditPosts) {
+
+            var redditContent = document.createElement('div');
+            document.body.appendChild(redditContent);
+            var titles = document.createElement('h3');
+                titles.innerText = redditPosts.title;
+                redditContent.appendChild(titles);
+            var attributes = document.createElement('a');
+                attributes.setAttribute('href', 'single.html?url=' + redditPosts.permalink);
+                attributes.appendChild(titles);
+                redditContent.appendChild(attributes);
+            var pics = document.createElement('img');
+                pics.setAttribute('src',redditPosts.url);
+                redditContent.appendChild(pics);
+        });
+    });
+
+    function contentInfo(content) {
+        var children = content.data.children;
+       
+    return children.map(function(child) {
+        var redditPosts = {};
+        redditPosts.thumbnail = child.data.thumbnail;
+        redditPosts.title = child.data.title;
+        redditPosts.url = child.data.url;
+        redditPosts.permalink = child.data.permalink;
+
+        return redditPosts;
+
+            
+        });
+    }
